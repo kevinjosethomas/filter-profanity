c06aed6327a46570c660b3d2a8593d2a31b9b715
TrustedMercury <kevin.jt2007@gmail.com>
2020-08-25 10:06:34 -0400

    Added MANIFEST.in to package data/profanity.json with the package

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/TrustedMercury/better-profanity",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
