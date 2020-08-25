083aad83473ed6a1822ffd960109cb88821eed64
TrustedMercury <kevin.jt2007@gmail.com>
2020-08-25 09:22:05 -0400

    Added censor method

--- profanity/profanity.py
+++ profanity/profanity.py
@@ -3,3 +3,9 @@ class Profanity:
 
     def __init__(self):
         pass
+
+    @staticmethod
+    def censor(text: str, censor_character="*": str) -> str:
+        """Censors obscene language in the text provided with censor_character"""
+
+        pass
