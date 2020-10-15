7f8a1853b1c75b7077c0cdd91d2aefc35211313a
TrustedMercury <kevin.jt2007@gmail.com>
2020-10-14 22:12:04 -0400

    Added tests for censor_profanity

---
+++ tests/test_censor.py
@@ -0,0 +1,9 @@
+import unittest
+
+from profanity import censor_profanity
+
+
+class TestCensor(unittest.TestCase):
+
+    def test_censor_false_small(self):
+        self.assertEqual(censor_profanity("hello"), "hello", "Program censored unnecesarily")
