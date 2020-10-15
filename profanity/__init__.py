1c13718a25ffdc84661055683ad1caf98babddad
TrustedMercury <kevin.jt2007@gmail.com>
2020-10-14 22:17:59 -0400

    Minor Bug Fixes

--- profanity/__init__.py
+++ profanity/__init__.py
@@ -11,16 +11,16 @@ with open(os.path.join(os.path.dirname(__file__), PROFANE_WORD_LIST_PATH), "r") 
 def censor_profanity(text: str, censor: str = "*"):
     """Censors all profane words with the provided censor"""
 
-    split_text = text.lower().split()
+    split_text = text.split()
     final_text = " ".join(split_text)
 
     if len(split_text) < len(PROFANE_WORD_LIST):
         for word in split_text:
-            if word in PROFANE_WORD_LIST:
+            if word.lower() in PROFANE_WORD_LIST:
                 final_text = final_text.replace(word, censor*len(word))
     else:
         for word in PROFANE_WORD_LIST:
-            if word in split_text:
+            if word.lower() in split_text:
                 final_text = final_text.replace(word, censor*len(word))
 
     return final_text
