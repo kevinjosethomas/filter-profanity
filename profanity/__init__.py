fd38f65fc867e84d261409d5b3a19f1e9f327d22
TrustedMercury <kevin.jt2007@gmail.com>
2020-10-14 22:07:23 -0400

    Added censor_profanity method

--- profanity/__init__.py
+++ profanity/__init__.py
@@ -6,3 +6,21 @@ from .config import PROFANE_WORD_LIST_PATH
 # Convert profane word list from json to array
 with open(os.path.join(__file__, PROFANE_WORD_LIST_PATH), "r") as _profane_word_list:
     PROFANE_WORD_LIST = json.load(_profane_word_list)
+
+
+def censor_profanity(text: str, censor: str = "*"):
+    """Censors all profane words with the provided censor"""
+
+    split_text = text.lower().split()
+    final_text = " ".join(split_text)
+
+    if len(split_text) < len(PROFANE_WORD_LIST):
+        for word in split_text:
+            if word in PROFANE_WORD_LIST:
+                final_text = final_text.replace(word, censor*len(word))
+    else:
+        for word in PROFANE_WORD_LIST:
+            if word in split_text:
+                final_text = final_text.replace(word, censor*len(word))
+
+    return final_text
