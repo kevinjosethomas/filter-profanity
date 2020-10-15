6b05bff0384c2612cd2e64a3d247113e0eb3f697
TrustedMercury <kevin.jt2007@gmail.com>
2020-10-14 22:16:17 -0400

    Added test executor

--- tests/test_censor.py
+++ tests/test_censor.py
@@ -7,3 +7,7 @@ class TestCensor(unittest.TestCase):
 
     def test_censor_false_small(self):
         self.assertEqual(censor_profanity("hello"), "hello", "Program censored unnecesarily")
+
+
+if __name__ == '__main__':
+    unittest.main()
