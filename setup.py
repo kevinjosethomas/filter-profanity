ad3bff3ed82f31ca2bc8389f47203d715140a7d6
TrustedMercury <kevin.jt2007@gmail.com>
2020-10-15 17:55:51 -0400

    Updated MANIFEST.in

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="filter-profanity",
-    version="1.0.6",
+    version="1.0.7",
     author="TrustedMercury",
     author_email="trustedmercury@gmail.com",
     description="The fastest python module to filter and censor obscene language from strings!",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/TrustedMercury/filter-profanity",
+    keywoords="profanity, filter, censor",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[
