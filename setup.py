3d706e784e34818c883f61c4113cd5dacf4addaf
TrustedMercury <kevin.jt2007@gmail.com>
2020-10-20 16:53:51 -0400

    Minor Bug Fixes

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/TrustedMercury/filter-profanity",
-    keywoords="profanity, filter, censor",
+    keywords="profanity, filter, censor",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[
